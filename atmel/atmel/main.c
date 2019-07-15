b66079eef7df1c6fdeaaf9b1aa3f6b23697d1ec2
S-VIN <7530023@gmail.com>
2019-07-15 19:06:24 +0300

    ADC

--- atmel/atmel/main.c
+++ atmel/atmel/main.c
@@ -10,7 +10,7 @@
 //PB4 - сигнал на пищалку
 //PB5 - ресет пин
 
-int ReadADC(){
+int ReadADC(int number_of_port){
 	// таблица 1
 	//  REFS2  REFS1  REFS0  - настраивает источник опорного напряжения
 	//    x      0      0    - VCC опорное напряжение
@@ -21,7 +21,24 @@ int ReadADC(){
 	//    1      1      1    - внутреннее 2.56В опорное напряжение с конденсатором на пине AREF
 	
 	// таблица 2
-	//   MUX3  MUX2
+	//  MUX3 MUX2 MUX1 MUX0 -  биты отвечающие за мультиплексор (к чему подключен АЦП)
+	//    0    0    0    0  - PB5
+	//    0    0    0    1  - PB2
+	//    0    0    1    0  - PB4
+	//    0    0    1    1  - PB3
+	// следующие нужны для калибровки (не использовать)
+	//    0    1    0    0  - PB4 PB4 x1
+	//    0    1    0    1  - PB4 PB4 x20
+	//    0    1    1    0  - PB4 PB3 x1
+	//    0    1    1    1  - PB4 PB3 x20
+	//    1    0    0    0  - PB5 PB5 x1
+	//    1    0    0    1  - PB5 PB5 x20
+	//    1    0    1    0  - PB5 PB2 x1
+	//    1    0    1    1  - PB5 PB2 x20
+	//    1    1    0    0  - Vbg
+	//    1    1    0    1  - GND
+	//    1    1    1    0  - N/A
+	//    1    1    1    1  - ADC4
 	
 	//           REFS1           - таблица 1
 	//           |REFS0          - таблица 1
@@ -32,19 +49,73 @@ int ReadADC(){
 	//           ||||||MUX1      - таблица 2
 	//           |||||||MUX0     - таблица 2
 	//           ||||||||
-	//ADCSRA = 0b10101110;
+	// ADMUX = 0b00000000;
+	ADMUX |= (0 << REFS1);
+	ADMUX |= (0 << REFS0);
+	ADMUX |= (0 << REFS1);
+	ADMUX |= (0 << REFS1);
+	ADMUX |= (1 << ADLAR);
+	
+	switch (number_of_port) {
+		case 5:
+			ADMUX |= (0 << MUX3);
+			ADMUX |= (0 << MUX2);
+			ADMUX |= (0 << MUX1);
+			ADMUX |= (0 << MUX0);
+		break;
+		
+		case 2:
+			ADMUX |= (0 << MUX3);
+			ADMUX |= (0 << MUX2);
+			ADMUX |= (0 << MUX1);
+			ADMUX |= (1 << MUX0);
+		break;
+		
+		case 4:
+			ADMUX |= (0 << MUX3);
+			ADMUX |= (0 << MUX2);
+			ADMUX |= (1 << MUX1);
+			ADMUX |= (0 << MUX0);
+		break;
+		
+		case 3:
+			ADMUX |= (0 << MUX3);
+			ADMUX |= (0 << MUX2);
+			ADMUX |= (1 << MUX1);
+			ADMUX |= (1 << MUX0);
+		break;
+		
+		default:
+			ADMUX |= (1 << MUX3);
+			ADMUX |= (1 << MUX2);
+			ADMUX |= (0 << MUX1);
+			ADMUX |= (1 << MUX0);
+		break;
+	}
+	
+	
 	
 	
 	//           ADEN           - Подать питание на АЦП
-	//           |ADSC          - Стартовать АЦП c инициализацией
+	//           |ADSC          - Запуск АЦП
 	//           ||ADATE        - Оцифровка по срабатываению триггера ADTS[2:0]
 	//           |||ADIF        - флаг прерывания от компаратора
 	//           ||||ADIE       - разрешение прерывания от компаратора
-	//           |||||ADPS[2:0] - Коэффициент делителя частоты АЦП K = 64
+	//           |||||ADPS2     - 
+	//           ||||||ADPS1    - 
+	//           |||||||ADPS0   - 
 	//           ||||||||
-	//ADCSRA = 0b10101110;
-	ADCSRA |= (1 << ADEN);
+	//ADCSRA = 0b10000111;
+	ADCSRA = 0b10000111;
+
+	ADCSRA |= (1 << ADSC);
 	
+	while(ADCSRA & (1 << ADSC));
+
+	int result = ADCH;
+	
+	ADCSRA |= (0 << ADEN);
+	return result;
 }
 
 inline void SetupPins(){
